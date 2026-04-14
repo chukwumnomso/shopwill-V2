a57d1d8dfb28bd823fcaeaa2ec3cb60926c79974
chinomso okoro <johnkennedychinomso@gmail.com>
2026-04-14 04:20:18 +0100

    fix: import file error

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import AppLayout from "./components/AppLayout";
 import MobileNav from "./components/MobileNav";
 import { NavBarProvider } from "./context/NavBarContext";
 import MenProductPage from "./pages/MenProductPage";
-import WomenProductPage from "./pages/WomenomenProductPage";
+import WomenProductPage from "./pages/womenProductPage";
 import { ModalProvider } from "./context/ModalContext";
 
 const router = createBrowserRouter([
